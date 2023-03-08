66f45dc3583798214308687ccae8e05d85a4a1b1
sakis475 <sakis475@gmail.com>
2023-03-09 00:55:16 +0200

    Boilerplate code

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { errorHandler } from './error_handler/error-handler';
+import cors from 'cors';
+import { apiEndpoints } from './routes/apiEndpoints';
+
+export const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+app.use('/v1', apiEndpoints);
+
+//express custom error handler
+app.use(errorHandler);
